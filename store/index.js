4fa823b07ce889e3102c9e526d03bfaf721a68a2
Vlad <kosachevlad@gmail.com>
2019-07-24 14:18:18 +0300

    implement chat layout

--- store/index.js
+++ store/index.js
@@ -1,4 +1,17 @@
-export const state = () => ({})
+export const state = () => ({
+  user: {},
+  messages: []
+})
+
+export const mutations = {
+  setUser(state, user) {
+    state.user = user
+  },
+  clearData(state) {
+    state.user = {}
+    state.messages = []
+  }
+}
 
 export const actions = {
   SOCKET_newMessage(ctx, data) {
